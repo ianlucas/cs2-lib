236e2df66eafc28bdc0b7906e24c358c277a4bd0
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-04-22 06:06:23 -0300

    sync

--- src/inventory-ui.ts
+++ src/inventory-ui.ts
@@ -2,10 +2,6 @@
  *  Copyright (c) Ian Lucas. All rights reserved.
  *--------------------------------------------------------------------------------------------*/
 
-import { CS_Economy } from "./economy";
-import { CS_Inventory } from "./inventory";
-import { CS_Team } from "./teams";
-
 interface CS_CategoryMenuItem {
     category: string;
     label: string;
@@ -49,92 +45,3 @@ export const CS_CATEGORY_MENU: CS_CategoryMenuItem[] = [
         unique: true
     }
 ];
-
-export class CS_InventoryUI extends CS_Inventory {
-    private getTypeFromCategory(category: string) {
-        const type = CS_Economy.items.find(
-            (item) => item.category === category
-        )?.type;
-        if (type === undefined) {
-            throw new Error("type not found");
-        }
-        return type;
-    }
-
-    static create() {
-        return new CS_Inventory().items;
-    }
-
-    getEquipped({ category, team }: { category: string; team: CS_Team }) {
-        const type = this.getTypeFromCategory(category);
-        if (type !== "weapon") {
-            const item = this.get({
-                item: { type },
-                team
-            });
-            if (item !== undefined && !item.unequipped) {
-                return [CS_Economy.getById(item.id)];
-            }
-            return [
-                CS_Economy.find({
-                    category,
-                    free: true,
-                    team,
-                    type
-                })
-            ];
-        }
-        return CS_Economy.filter({ type, category, free: true, team }).map(
-            (defaultItem) => {
-                const item = this.get({
-                    item: {
-                        model: defaultItem.model,
-                        type
-                    },
-                    team
-                });
-                if (item !== undefined && !item.unequipped) {
-                    return CS_Economy.getById(item.id);
-                }
-                return defaultItem;
-            }
-        );
-    }
-
-    getEquippable({
-        category,
-        model,
-        team
-    }: {
-        category: string;
-        model?: string;
-        team: CS_Team;
-    }) {
-        const type = this.getTypeFromCategory(category);
-        const item = this.get({
-            item: { type, model },
-            team
-        });
-        const isGlove = type === "glove";
-        if (item && CS_Inventory.isWithinLocktime(item.locktime)) {
-            return [
-                CS_Economy.find({
-                    category,
-                    free: true,
-                    model,
-                    team,
-                    type
-                }),
-                CS_Economy.getById(item.id)
-            ];
-        }
-        return CS_Economy.filter({
-            base:
-                model && !isGlove ? undefined : model && isGlove ? false : true,
-            category,
-            model,
-            team,
-            type
-        });
-    }
-}
--- src/inventory.ts
+++ src/inventory.ts
@@ -49,6 +49,16 @@ export class CS_Inventory {
         this.items = items;
     }
 
+    private getTypeFromCategory(category: string) {
+        const type = CS_Economy.items.find(
+            (item) => item.category === category
+        )?.type;
+        if (type === undefined) {
+            throw new Error("type not found");
+        }
+        return type;
+    }
+
     get({ item, team }: { item: Partial<CS_Item>; team: CS_Team }) {
         return this.items.find((equipped) => {
             const other = CS_Economy.getById(equipped.id);
@@ -162,5 +172,78 @@ export class CS_Inventory {
         } catch {
             return this.items;
         }
+    }
+
+    getEquipped({ category, team }: { category: string; team: CS_Team }) {
+        const type = this.getTypeFromCategory(category);
+        if (type !== "weapon") {
+            const item = this.get({
+                item: { type },
+                team
+            });
+            if (item !== undefined && !item.unequipped) {
+                return [CS_Economy.getById(item.id)];
+            }
+            return [
+                CS_Economy.find({
+                    category,
+                    free: true,
+                    team,
+                    type
+                })
+            ];
+        }
+        return CS_Economy.filter({ type, category, free: true, team }).map(
+            (defaultItem) => {
+                const item = this.get({
+                    item: {
+                        model: defaultItem.model,
+                        type
+                    },
+                    team
+                });
+                if (item !== undefined && !item.unequipped) {
+                    return CS_Economy.getById(item.id);
+                }
+                return defaultItem;
+            }
+        );
+    }
+
+    getEquippable({
+        category,
+        model,
+        team
+    }: {
+        category: string;
+        model?: string;
+        team: CS_Team;
+    }) {
+        const type = this.getTypeFromCategory(category);
+        const item = this.get({
+            item: { type, model },
+            team
+        });
+        const isGlove = type === "glove";
+        if (item && CS_Inventory.isWithinLocktime(item.locktime)) {
+            return [
+                CS_Economy.find({
+                    category,
+                    free: true,
+                    model,
+                    team,
+                    type
+                }),
+                CS_Economy.getById(item.id)
+            ];
+        }
+        return CS_Economy.filter({
+            base:
+                model && !isGlove ? undefined : model && isGlove ? false : true,
+            category,
+            model,
+            team,
+            type
+        });
     }
 }
