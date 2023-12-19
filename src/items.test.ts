7b25e3e23a284f793ddcb14164f838f3c01fae88
Ian Lucas <ianlucas_ferreira@hotmail.com>
2023-12-19 19:18:54 -0300

    chore(test): add tests for checking missing items

--- src/items.test.ts
+++ src/items.test.ts
@@ -1,10 +1,15 @@
+import { CS_Item } from "./economy";
 import { CS_ITEMS } from "./items";
+import fetch from "node-fetch";
 
-test("has every weapon categories", () => {
+const itemMap = new Map<number, CS_Item>();
+
+test("has every types and categories", () => {
     const weaponCategories = new Set<string>();
     const itemTypes = new Set<string>();
 
     for (const item of CS_ITEMS) {
+        itemMap.set(item.id, item);
         if (item.type === "weapon") {
             if (item.category === undefined) {
                 throw new Error("weapon with category undefined.");
@@ -29,4 +34,23 @@ test("has every weapon categories", () => {
         "key",
         "tool"
     ]);
+});
+
+test("compare repository items with current items", async () => {
+    const repositoryItems = (await (
+        await fetch("https://raw.githubusercontent.com/ianlucas/cslib/main/dist/items.json")
+    ).json()) as CS_Item[];
+
+    for (const repItem of repositoryItems) {
+        const item = itemMap.get(repItem.id);
+        if (item === undefined) {
+            throw new Error(`item not found.`);
+        }
+        expect(item.type).toBe(repItem.type);
+        if (item.type === "weapon") {
+            expect(item.category).toBe(repItem.category);
+        }
+        expect(item.def).toBe(repItem.def);
+        expect(item.index).toBe(repItem.index);
+    }
 });
