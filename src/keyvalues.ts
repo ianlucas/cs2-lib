ff481de507690583b22750c6119e772ecd80c7fc
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-03-21 10:07:54 -0300

    fix(keyvalues): use extension in import

--- src/keyvalues.ts
+++ src/keyvalues.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { fail } from "./util";
+import { fail } from "./util.js";
 
 type KeyValue = [string, string | KeyValue[]];
 
