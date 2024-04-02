dfd2823fad7446146e9b0641e8d7ad51858ba9c1
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-04-02 14:59:09 -0300

    fix: safe wrapper in class context

--- src/util.ts
+++ src/util.ts
@@ -9,16 +9,6 @@ export function compare<T, U>(var1: T, var2: U): boolean {
     return var1 === undefined || var1 === (typeof var1 === "boolean" ? var2 || false : var2);
 }
 
-export function safe<T extends (...args: any[]) => any>(fn: T): (...args: Parameters<T>) => ReturnType<T> | false {
-    return function safe(...args: Parameters<T>): ReturnType<T> | false {
-        try {
-            return fn(...args);
-        } catch {
-            return false;
-        }
-    };
-}
-
 export function float(literal: number, fractionDigits: number = 2) {
     return parseFloat(literal.toFixed(fractionDigits));
 }
