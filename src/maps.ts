a03f27ba2dc8a83faff322215a1740e1414bed20
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-05-14 09:52:17 -0300

    feat: update active map pool

--- src/maps.ts
+++ src/maps.ts
@@ -2,85 +2,66 @@
  *  Copyright (c) Ian Lucas. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+import { assert } from "./util";
+
 export interface CS_Map {
     mapname: string;
     name: string;
-    image: string;
 }
 
 export const CS_ANCIENT_MAP: CS_Map = {
     mapname: "de_ancient",
-    name: "Ancient",
-    image: "/de_ancient.jpg"
+    name: "Ancient"
 };
 
 export const CS_ANUBIS_MAP: CS_Map = {
     mapname: "de_anubis",
-    name: "Anubis",
-    image: "/de_anubis.jpg"
+    name: "Anubis"
 };
 
 export const CS_INFERNO_MAP: CS_Map = {
     mapname: "de_inferno",
-    name: "Inferno",
-    image: "/de_inferno.jpg"
+    name: "Inferno"
 };
 
 export const CS_MIRAGE_MAP: CS_Map = {
     mapname: "de_mirage",
-    name: "Mirage",
-    image: "/de_mirage.jpg"
+    name: "Mirage"
 };
 
 export const CS_DUST2_MAP: CS_Map = {
     mapname: "de_dust2",
-    name: "Dust 2",
-    image: "/de_dust2.jpg"
+    name: "Dust 2"
 };
 
 export const CS_NUKE_MAP: CS_Map = {
     mapname: "de_nuke",
-    name: "Nuke",
-    image: "/de_nuke.jpg"
+    name: "Nuke"
 };
 
 export const CS_OVERPASS_MAP: CS_Map = {
     mapname: "de_overpass",
-    name: "Overpass",
-    image: "/de_overpass.jpg"
+    name: "Overpass"
 };
 
 export const CS_VERTIGO_MAP: CS_Map = {
     mapname: "de_vertigo",
-    name: "Vertigo",
-    image: "/de_vertigo.jpg"
+    name: "Vertigo"
 };
 
 export const CS_TRAIN_MAP: CS_Map = {
     mapname: "de_train",
-    name: "Train",
-    image: "/de_train.jpg"
-};
-
-export const CS_OLD_CBBLE_MAP: CS_Map = {
-    mapname: "workshop/855577410/de_cbble",
-    name: "Cobblestone",
-    image: "/855577410_de_cbble.jpg"
-};
-
-export const CS_OLD_CACHE_MAP: CS_Map = {
-    mapname: "workshop/951327114/de_cache",
-    name: "Cache",
-    image: "/951327114_de_cache.jpg"
+    name: "Train"
 };
 
 export const CS_ACTIVE_MAP_POOL = [
     CS_ANCIENT_MAP,
     CS_ANUBIS_MAP,
+    CS_DUST2_MAP,
     CS_INFERNO_MAP,
     CS_MIRAGE_MAP,
     CS_NUKE_MAP,
-    CS_OVERPASS_MAP,
     CS_VERTIGO_MAP
 ];
 
@@ -92,21 +73,12 @@ export const CS_ALL_MAPS = [
     CS_MIRAGE_MAP,
     CS_NUKE_MAP,
     CS_TRAIN_MAP,
-    CS_OLD_CACHE_MAP,
-    CS_OLD_CBBLE_MAP,
     CS_OVERPASS_MAP,
     CS_VERTIGO_MAP
 ];
 
-export function CS_getChangeLevelCommand(map: CS_Map | string): string {
-    const mapstring = typeof map === "string" ? map : map.mapname;
-    const matches = mapstring.match(/workshop\/(\d+)\/[\w_]+/);
-    if (matches) {
-        return `host_workshop_map ${matches[1]}`;
-    }
-    return `changelevel ${mapstring}`;
-}
-
 export function CS_getMapnameName(mapname: string): string {
-    return CS_ALL_MAPS.find((map) => map.mapname.includes(mapname))?.name ?? "undefined";
+    const name = CS_ALL_MAPS.find((map) => map.mapname.includes(mapname))?.name;
+    assert(name !== undefined, `Mapname ${mapname} not found in CS_ALL_MAPS`);
+    return name;
 }
