537ad013e2179a93a0c8d205fbc656aba52f2452
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-05-29 22:29:16 -0300

    fix: add missing `.js` extension

--- src/maps.ts
+++ src/maps.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ensure } from "./utils";
+import { ensure } from "./utils.js";
 
 export interface CS2Map {
     mapname: string;
