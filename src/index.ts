6d1657b3dbf4d73f4d1b276795a336e0b82a3f60
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-05-29 21:19:50 -0300

    feat: export more modules

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+export * from "./economy-constants.js";
 export * from "./economy-container.js";
+export * from "./economy-types.js";
 export * from "./economy.js";
 export * from "./inventory.js";
 export * from "./items.js";
 export * from "./keyvalues.js";
 export * from "./maps.js";
 export * from "./teams.js";
+export * from "./utils.js";
 export * from "./veto.js";
