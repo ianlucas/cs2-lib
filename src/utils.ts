e321be091c23f7bec5577344c1438636727c405e
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-05-31 14:52:20 -0300

    feat: add utilities for stickers and patches

--- src/utils.ts
+++ src/utils.ts
@@ -14,6 +14,8 @@ export type Interface<T extends object> = {
         | undefined;
 };
 
+export type MapValue<T> = T extends Map<any, infer I> ? I : never;
+
 export function compare<T, U>(var1: T, var2: U): boolean {
     return var1 === undefined || var1 === (typeof var1 === "boolean" ? var2 || false : var2);
 }
