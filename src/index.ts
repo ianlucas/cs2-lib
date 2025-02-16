0d0dce2639b44a959731825a3cfa20c95d3d733b
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-02-16 12:37:31 -0300

    add keyvalues3 parser

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from "./economy.js";
 export * from "./inventory.js";
 export * from "./items.js";
 export * from "./keyvalues.js";
+export * from "./keyvalues3.js";
 export * from "./maps.js";
 export * from "./teams.js";
 export * from "./utils.js";
---
+++ src/keyvalues3.test.ts
@@ -0,0 +1,41 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { expect, test } from "vitest";
+import { CS2KeyValues3 } from "./keyvalues3";
+
+test("keyvalues3 basics", () => {
+    expect(
+        CS2KeyValues3.parse(`<!-- kv3 encoding:text:version{e21c7f3c-8a33-41c5-9977-a76d3a32aa0d} format:generic:version{7412167c-06e9-4698-aff2-e63eb59037e7} -->
+{
+	boolValue = false
+	intValue = 128
+	doubleValue = 64.000000
+	stringValue = "hello world"
+	stringThatIsAResourceReference = resource:"particles/items3_fx/star_emblem.vpcf"
+	arrayValue =
+	[
+		1,
+		2,
+	]
+	objectValue =
+	{
+		n = 5
+		s = "foo"
+	}
+}`)
+    ).toStrictEqual({
+        boolValue: false,
+        intValue: 128,
+        doubleValue: 64,
+        stringValue: "hello world",
+        stringThatIsAResourceReference: "resource:particles/items3_fx/star_emblem.vpcf",
+        arrayValue: [1, 2],
+        objectValue: {
+            n: 5,
+            s: "foo"
+        }
+    });
+});
