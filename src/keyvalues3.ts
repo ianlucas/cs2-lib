4382109310e5c708e3e6bde103c34d204bebea3f
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-02-16 12:39:59 -0300

    feat: add keyvalues3 parser (#63)

---
+++ src/keyvalues3.ts
@@ -0,0 +1,201 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { fail } from "./utils.js";
+
+type CS2KeyValue3Value = null | string | boolean | number | CS2KeyValue3Value[] | { [key: string]: CS2KeyValue3Value };
+
+export class CS2KeyValues3 {
+    static parse<T = any>(data: string): T {
+        data = data.replace(/\[[\$!][^\]]+\]/g, "").trim();
+        let index = 0;
+
+        function skipHeader(): void {
+            if (data.startsWith("<!--")) {
+                const endIndex = data.indexOf("-->");
+                if (endIndex !== -1) {
+                    index = endIndex + 3;
+                    skipWhitespace();
+                }
+            }
+        }
+
+        function isWhitespace(char?: string): boolean {
+            return (char?.match(/[\s\t\r\n]/) ?? undefined) !== undefined;
+        }
+
+        function isNumber(char?: string): boolean {
+            return (char?.match(/[\d-]/) ?? undefined) !== undefined;
+        }
+
+        function skipWhitespace(): void {
+            while (data[index] && isWhitespace(data[index])) {
+                index += 1;
+            }
+            if (data[index] === "/" && data[index + 1] === "/") {
+                while (data[index] && data[index] !== "\n") {
+                    index += 1;
+                }
+                skipWhitespace();
+            }
+        }
+
+        function parseKey(): string {
+            let value = "";
+            while (data[index]) {
+                if (data[index]?.match(/[a-zA-Z_\d\.]/)) {
+                    value += data[index];
+                    index += 1;
+                } else {
+                    if (isWhitespace(data[index])) {
+                        skipWhitespace();
+                    }
+                    if (data[index] === "=") {
+                        if (value.length === 0) {
+                            fail(`Empty key at ${index}`);
+                        }
+                        index += 1;
+                        return value;
+                    }
+                    fail(`Bad end of key at ${index}`);
+                }
+            }
+            fail(`Bad end of key at ${index}`);
+        }
+
+        function parseString(): string {
+            if (data[index] === '"') {
+                index += 1;
+                let value = "";
+                while (data[index] && data[index] !== '"') {
+                    while (data[index] && data[index] === "\\") {
+                        index += 1;
+                        const char = data[index];
+                        if (char === "n") {
+                            value += "\n";
+                        } else {
+                            value += char;
+                        }
+                        index += 1;
+                    }
+                    if (data[index] !== '"') {
+                        value += data[index];
+                        index += 1;
+                    }
+                }
+                if (data[index] !== '"') {
+                    fail("Bad end of string");
+                }
+                index += 1;
+                return value;
+            }
+            return "";
+        }
+
+        function parseNumber(): number {
+            let value = "";
+            while (data[index]) {
+                if (isNumber(data[index]) || data[index] === ".") {
+                    value += data[index];
+                    index += 1;
+                } else {
+                    const n = Number(value);
+                    if (Number.isNaN(n)) {
+                        fail(`Invalid number at ${index}`);
+                    }
+                    return n;
+                }
+            }
+            fail("Bad end of number");
+        }
+
+        function parseValue(): CS2KeyValue3Value {
+            if (data.slice(index).startsWith("null")) {
+                index += 4;
+                return null;
+            }
+            if (data.slice(index).startsWith("true")) {
+                index += 4;
+                return true;
+            }
+            if (data.slice(index).startsWith("false")) {
+                index += 5;
+                return false;
+            }
+            if (data.slice(index).startsWith("resource:")) {
+                index += 9;
+                return `resource:${parseString()}`;
+            }
+            if (isNumber(data[index])) {
+                return parseNumber();
+            }
+            if (data[index] === '"') {
+                return parseString();
+            }
+            if (data[index] === "[") {
+                index += 1;
+                return parseArray();
+            }
+            if (data[index] === "{") {
+                index += 1;
+                return parsePairs();
+            }
+            if (data[index] === "}") {
+                return "";
+            }
+
+            console.log(
+                data.substring(Math.max(0, index - 64), index) +
+                    data[index] +
+                    data.substring(index + 1, Math.min(data.length, index + 63))
+            );
+            console.log("".padStart(64, " ") + "^");
+            fail(`Unexpected character at index ${index}`);
+        }
+
+        function parseArray(): CS2KeyValue3Value[] {
+            const values: CS2KeyValue3Value[] = [];
+            while (data[index]) {
+                skipWhitespace();
+                if (data[index] === "]") {
+                    index += 1;
+                    return values;
+                }
+                skipWhitespace();
+                if (data[index] === ",") {
+                    index += 1;
+                    skipWhitespace();
+                    continue;
+                }
+                values.push(parseValue());
+            }
+            return values;
+        }
+
+        function parsePairs(): { [key: string]: CS2KeyValue3Value } {
+            const result: { [key: string]: CS2KeyValue3Value } = {};
+            while (data[index]) {
+                if (data[index] === "{") {
+                    index += 1;
+                    continue;
+                }
+                if (data[index] === "}") {
+                    index += 1;
+                    return result;
+                }
+                skipWhitespace();
+                const key = parseKey();
+                skipWhitespace();
+                const value = parseValue();
+                skipWhitespace();
+                result[key] = value;
+            }
+            return result;
+        }
+
+        skipHeader();
+        return parsePairs() as T;
+    }
+}
