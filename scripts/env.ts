90b972baa159a87d93bdc722026bfdaffff7f838
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-04-03 20:53:29 -0300

    feat: add fever case specials (#90)

--- scripts/env.ts
+++ scripts/env.ts
@@ -7,4 +7,4 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const CS2_CSGO_PATH = process.env.CS2_CSGO_PATH ?? `${process.cwd()}/scripts/workdir/decompiled`;
-export const { CS2_CSGO_FULL_PATH, INPUT_FORCE } = process.env;
+export const { CS2_CSGO_FULL_PATH, CS2_CSGO_DIRECTORY_PATH, INPUT_FORCE } = process.env;
