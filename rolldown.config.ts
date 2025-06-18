3e2bc9ebe68762880b5d6a2079965dc17f8e730a
Andrii Yaroshenko <lavilaya@protonmail.ch>
2025-06-18 11:14:08 +0000

    chore: unified rolldown config file formatting

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -3,19 +3,19 @@ import { dts } from 'rolldown-plugin-dts';
 import del from 'rollup-plugin-delete'
 
 export default defineConfig({
-  input: ['src/index.ts', 'src/translations/index.ts'],
-	plugins: [
-		del({ targets: ['dist'] }),
-		dts(),
-	],
-	treeshake: true,
-  output: {
-		dir: 'dist',
-		format: 'esm',
-		minify: true,
-		entryFileNames: '[name].mjs',
-		chunkFileNames: '_chunks/[name]-[hash].mjs',
-		preserveModules: true,
-		preserveModulesRoot: 'src'
-  },
+    input: ['src/index.ts', 'src/translations/index.ts'],
+    plugins: [
+        del({ targets: ['dist'] }),
+        dts(),
+    ],
+    treeshake: true,
+    output: {
+        dir: 'dist',
+        format: 'esm',
+        minify: true,
+        entryFileNames: '[name].mjs',
+        chunkFileNames: '_chunks/[name]-[hash].mjs',
+        preserveModules: true,
+        preserveModulesRoot: 'src'
+    },
 });
