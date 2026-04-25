291e2a3ee0936c501bc43f9a3535584a00eacbf3
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-04-25 09:36:02 -0300

    Missing copyright banner

--- scripts/sharp.d.ts
+++ scripts/sharp.d.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 declare module "sharp" {
     import sharp = require("../node_modules/sharp/lib/index.d.ts");
 
