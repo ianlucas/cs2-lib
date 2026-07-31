f23162ce91a06cc7094fc5d5c9838cddd6c362ea
Ian Lucas <ianlucas_ferreira@hotmail.com>
2026-07-31 18:36:53 -0300

    Pin asset pipeline dependencies to keep generated asset hashes stable (#458)

--- scripts/item-generator/Catalog/SourceDataLoader.cs
+++ scripts/item-generator/Catalog/SourceDataLoader.cs
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using ItemGenerator.GameFiles;
